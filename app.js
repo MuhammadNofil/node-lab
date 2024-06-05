cb86be5bd7d8607e177548d9f7146931dd1b1d1a
MuhammadNofil <muhammadnofil1290@gmail.com>
2024-06-05 10:33:00 +0500

    changes

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ const dbConnection = async () => {
 
 // inititalizng the server
 
-app.listen(3000, () => {
-    console.log('server is listening on port 3000')
+app.listen(4000, () => {
+    console.log('server is listening on port 4000')
     // calling the db function
     dbConnection()
 })
